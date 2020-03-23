51fd45082c6196fc202db854e980c22a903228c5
verianggoro <veryanggoro007@gmail.com>
2020-03-23 21:25:51 +0700

    Initial Commit

---
+++ Pembelajar Beta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pembelajar Beta
+//
+//  Created by Veri Anggoro on 23/03/20.
+//  Copyright © 2020 Veri Anggoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
